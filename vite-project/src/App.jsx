b4275e56eea4f7d9997c16153e5dd2bb1676c6e6
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2025-01-06 22:52:03 -0300

    atualizado

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Site from './assets/site';  // Certifique-se de que o caminho do arquivo está correto
+
+function App() {
+  return (
+    <div>
+      <Site />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/assets/Site.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Site () {
+  return (
+    <div><h1>Olá mundo</h1></div>
+  )
+}
+
+export default Site
