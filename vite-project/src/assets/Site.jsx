06f5024bb167d87d40082bf9e3d7fbefe3ab638c
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2025-01-12 21:44:45 -0300

    atualizei o projeto

--- vite-project/src/assets/Site.jsx
+++ vite-project/src/assets/Site.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Site () {
   return (
-    <div><h1>Max Verstappen</h1></div>
+    <div id='container'>
+      <h1>Max verstappen</h1>
+      <header id='imagem'></header>
+      
+      <div id='verstappen'>
+        <p>Max Emilian Verstappen é um automobilista neerlandês. Ele é piloto de Fórmula 1 desde 2015, tendo feito sua estreia pela equipe Toro Rosso. Foi quatro vezes campeão da Fórmula 1 em 2021, 2022, 2023 e 2024 e terminou em terceiro lugar em 2019 e 2020 com a Red Bull Racing, equipe pela qual corre desde 2016</p>
+      </div>
+    </div>
   )
 }
 
